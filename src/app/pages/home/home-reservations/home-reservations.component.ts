edec501ba3c041a6012ea2641c4356fc739e5bbf
prophet-99 <alexanderavilab@gmail.com>
2025-11-01 22:26:19 -0500

    feat: implement form modal

--- src/app/pages/home/home-reservations/home-reservations.component.ts
+++ src/app/pages/home/home-reservations/home-reservations.component.ts
@@ -1,4 +1,5 @@
-import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
 import {
   FormGroup,
   FormBuilder,
@@ -8,7 +9,7 @@ import {
 import { ButtonComponent, ModalComponent } from '@shared/components';
 import { ModalEventBusService } from '@shared/components/modal/modal.event-bus.service';
 
-const NG_IMPORTS = [ReactiveFormsModule];
+const NG_IMPORTS = [ReactiveFormsModule, CommonModule];
 const IMPORTS = [ButtonComponent, ModalComponent];
 
 @Component({
@@ -17,18 +18,60 @@ const IMPORTS = [ButtonComponent, ModalComponent];
   templateUrl: './home-reservations.component.html',
   styleUrl: './home-reservations.component.scss',
 })
-export class HomeReservationsComponent {
+export class HomeReservationsComponent implements OnInit {
   // DI
   private readonly fb = inject(FormBuilder);
   private readonly modalEventBusService = inject(ModalEventBusService);
   // LOCALE
   reservationForm!: FormGroup;
+  modalReservationForm!: FormGroup;
 
   constructor() {
     this.reservationForm = this.fb.group({
       dateReservation: ['2025-09-25', [Validators.required]],
       timeReservation: ['20:00', [Validators.required]],
     });
+
+    this.modalReservationForm = this.fb.group({
+      dateReservation: [
+        this.reservationForm.get('dateReservation')?.value,
+        [Validators.required],
+      ],
+      timeReservation: [
+        this.reservationForm.get('timeReservation')?.value,
+        [Validators.required],
+      ],
+      fullName: ['', [Validators.required]],
+      lastName: ['', [Validators.required]],
+      age: ['', [Validators.required, Validators.min(1)]],
+      address: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      numberSeats: [
+        0,
+        [Validators.required, Validators.min(1), Validators.max(4)],
+      ],
+    });
+  }
+
+  ngOnInit(): void {
+    // TODO: EVITAR MEMORY LEAKS
+    this.reservationForm.valueChanges.subscribe(
+      ({ dateReservation, timeReservation }) => {
+        this.modalReservationForm.patchValue({
+          dateReservation,
+          timeReservation,
+        });
+      }
+    );
+
+    this.modalReservationForm.valueChanges.subscribe(
+      ({ dateReservation, timeReservation }) => {
+        this.reservationForm.patchValue({
+          dateReservation,
+          timeReservation,
+        });
+      }
+    );
   }
 
   onReserve() {
